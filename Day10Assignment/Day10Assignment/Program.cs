ae8b78c0d9421888271becd8c3a5ef79dc3a3662
Balla <ballaphanindra@outlook.com>
2022-06-22 17:09:10 +0530

    updating files

--- Day10Assignment/Day10Assignment/Program.cs
+++ Day10Assignment/Day10Assignment/Program.cs
@@ -12,6 +12,7 @@ namespace Day10Assignment
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
+       
                 Console.WriteLine("Employee is present");
             }
             else
