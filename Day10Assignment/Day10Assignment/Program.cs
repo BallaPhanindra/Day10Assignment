6a0728c4d00229da1eb527a9261c47ed787b3826
Balla <ballaphanindra@outlook.com>
2022-06-22 17:07:14 +0530

    updated

--- Day10Assignment/Day10Assignment/Program.cs
+++ Day10Assignment/Day10Assignment/Program.cs
@@ -6,18 +6,28 @@ namespace Day10Assignment
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage is " + empWage);
         }
     }
 }
